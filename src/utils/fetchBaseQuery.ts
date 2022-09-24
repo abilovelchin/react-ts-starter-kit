68021ccd6838f556dad11b4c92e7687f5d8892ff
Elchin Abilov <abilovelchin@gmail.com>
2022-09-24 17:33:38 +0400

    .

--- src/utils/fetchBaseQuery.ts
+++ src/utils/fetchBaseQuery.ts
@@ -1,14 +1,14 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
-import type { RootState } from "$store/index";
 
 export const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_APP_API,
-  prepareHeaders: (headers, { getState }) => {
-    const { token } = (getState() as RootState).auth.user;
-
+  prepareHeaders: (headers) => {
     // If we have a token set in state, let's assume that we should be passing it.
-    if (token) {
-      headers.set("authorization", `Bearer ${token}`);
+    if (localStorage.getItem("starter:token")) {
+      headers.set(
+        "authorization",
+        `Bearer ${localStorage.getItem("starter:token")}`
+      );
     }
 
     return headers;
