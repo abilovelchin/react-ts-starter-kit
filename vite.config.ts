f54aebec20427e7bacb3dfdb53734c04d4cfaae3
Elchin Abilov <abilovelchin@gmail.com>
2025-02-19 11:39:29 +0400

    refactor: update Tailwind CSS configuration and styles for compatibility with v4

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react';
 import generouted from '@generouted/react-router/plugin';
 import autoImportComponents from './src/utils/vite-plugin-auto-import-components';
 
+// @ts-ignore
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), generouted(), autoImportComponents()],
+  plugins: [react(), generouted(), autoImportComponents(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve('src'),
